1eeacdcceb6593eb5a2bded9e29c5487846ae859
Fernando Vargas <fervargas2210@gmail.com>
2026-09-13 03:09:53 -0300

    FIX - Ajustado para aceitar a url de produção

--- Program.cs
+++ Program.cs
@@ -13,7 +13,8 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "http://localhost:5173",
-                "https://localhost:7151"
+                "https://localhost:7151",
+                "https://bc-bistro.vercel.app"
             )
             .AllowAnyHeader()
             .AllowAnyMethod()
